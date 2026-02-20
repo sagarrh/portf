fbef3c3ec8f0adde3097860f5458e6b54a46cc04
Sagar Harsora <142764820+sagarrh@users.noreply.github.com>
2026-02-20 12:32:13 +0530

    Responsive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { Link as LinkIcon } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <div className="flex h-screen items-center justify-center pb-40 text-xl from-neutral-950">
+    <div className="flex min-h-screen items-center justify-center px-4 sm:px-6 text-xl from-neutral-950">
       404 - Not Found :( <br /> The page you are looking for does not exist.
       <Link className="ml-2 text-blue-500 hover:underline" href="/"><LinkIcon className="inline mr-1" size={16} /></Link>
       
